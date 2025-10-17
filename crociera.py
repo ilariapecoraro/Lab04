291447cda73f35b1c9863efbc28948951c592204
ilariapecoraro <pecoraroilaria6@gmail.com>
2025-10-17 17:33:52 +0200

    SOLUZIONE

--- crociera.py
+++ crociera.py
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-class Crociera:
-    def __init__(self, nome):
-        """Inizializza gli attributi e le strutture dati"""
-        # TODO
 
-    """Aggiungere setter e getter se necessari"""
-    # TODO
-
-    def carica_file_dati(self, file_path):
-        """Carica i dati (cabine e passeggeri) dal file"""
-        # TODO
-
-    def assegna_passeggero_a_cabina(self, codice_cabina, codice_passeggero):
-        """Associa una cabina a un passeggero"""
-        # TODO
-
-    def cabine_ordinate_per_prezzo(self):
-        """Restituisce la lista ordinata delle cabine in base al prezzo"""
-        # TODO
-
-
-    def elenca_passeggeri(self):
-        """Stampa l'elenco dei passeggeri mostrando, per ognuno, la cabina a cui è associato, quando applicabile """
-        # TODO
-
-=======
 import operator
 from passeggero import Passeggero
 from cabina import Cabina
@@ -139,4 +113,4 @@ class Crociera: # inizializza la classe crociera
         """Stampa l'elenco dei passeggeri mostrando, per ognuno, la cabina a cui è associato, quando applicabile """
         # TODO
 
->>>>>>> Add files via upload
+
